1461dfd7addec7612bdf5d15ed63060b912c3fd4
ManuelHueitra <manuel.hueitra@alumnos.ulagos.cl>
2025-12-04 12:30:29 -0400

    arreglo del main

--- src/main/java/patagonia/Main.java
+++ src/main/java/patagonia/Main.java
@@ -1,81 +1,80 @@
 package patagonia;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import patagonia.model.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main extends Application
-{
-    public static List<Usuario> ListaUsuarios = new ArrayList<Usuario>();
-    public static List<Embarcacion> ListaEmbarcaciones = new ArrayList<Embarcacion>();
-    public static List<Destino> ListaDestinos = new ArrayList<Destino>();
-    //Lista para guardar los viajes creados
-    public static List<Viaje> ListaViajes = new ArrayList<>();
-    public void start(Stage primaryStage) throws Exception
-    {
-        Parent root = FXMLLoader.load(getClass().getResource("/patagonia/Menu_Inicio.fxml"));
+public class Main extends Application {
+
+    //listas globales 
+    public static List<Usuario> listaUsuarios = new ArrayList<>();
+    public static List<Embarcacion> listaEmbarcaciones = new ArrayList<>();
+    public static List<Destino> listaDestinos = new ArrayList<>();
+    public static List<Viaje> listaViajes = new ArrayList<>();
+    public static List<Encomienda> listaEncomiendas = new ArrayList<>(); 
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //carga el menú principal
+        Parent root = FXMLLoader.load(getClass().getResource("/patagonia/view/MenuPrincipal.fxml"));
+        
         primaryStage.setTitle("Patagonia Wellboat - Acceso");
-        primaryStage.setScene(new Scene(root, 700, 600));
+        primaryStage.setScene(new Scene(root)); 
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
-        // Intentamos cargar datos del archivo primero
-        boolean cargado = GestorArchivos.cargarDatos(ListaUsuarios, ListaDestinos, ListaEmbarcaciones, ListaViajes);
-        // Si no había archivo usamos tus datos por defecto
+        // intentamos cargar los datos del archivo
+        boolean cargado = GestorArchivos.cargarDatos(listaUsuarios, listaDestinos, listaEmbarcaciones, listaViajes, listaEncomiendas);
+
+        // si no existen datos previos creamos los por defecto
         if (!cargado) {
             cargarDatosIniciales();
-            // Y guardamos de inmediato para crear el archivo
-            GestorArchivos.guardarDatos(ListaUsuarios, ListaDestinos, ListaEmbarcaciones, ListaViajes);
+            guardarCambios(); // Crea el archivo por primera vez
         }
         launch(args);
     }
-    private static void cargarDatosIniciales(){
-        System.out.println("Cargando datos...");
-        ListaUsuarios.add(new Usuario("admin", "admin123", "Administrador")); //contraseña por admin
 
-        // lista de destinos
-        ListaDestinos.add(new Destino("Calbuco", 15000, 4000));
-        ListaDestinos.add(new Destino("Maillen", 15000, 4000));
-        ListaDestinos.add(new Destino("Isla Puluqui", 20000,5000));
-        ListaDestinos.add(new Destino("Achao", 25000, 6000));
-        ListaDestinos.add(new Destino("Quinchao", 25000, 6000));
-        ListaDestinos.add(new Destino("Dalcahue", 30000, 8000));
-        ListaDestinos.add(new Destino("Curaco de Vélez", 30000, 8000));
-        ListaDestinos.add(new Destino("Queilen", 40000, 10000));
-        ListaDestinos.add(new Destino("Chonchi", 40000, 10000));
-        ListaDestinos.add(new Destino("Melinka", 50000, 12000));
-        ListaDestinos.add(new Destino("Quellón", 50000, 12000));
-        //lista de barcos
-        ListaEmbarcaciones.add(new CatamaranLiviano("CAT-001"));
-        ListaEmbarcaciones.add(new FerryMediano("FER-002"));
-        ListaEmbarcaciones.add(new WellboatGranCapacidad("WEL-003"));
-        System.out.println("Datos cargados correctamente: " + ListaUsuarios.size() + " usuarios, " + ListaDestinos.size() + " destinos.");
+    public static void guardarCambios() {
+        GestorArchivos.guardarDatos(listaUsuarios, listaDestinos, listaEmbarcaciones, listaViajes, listaEncomiendas);
     }
-    @FXML
-    void irVistaCliente(ActionEvent event) {
-        try {
-            // Cargar la vista de Login de Cliente (la verde)
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/patagonia/Menu_Cliente.fxml"));
-            Parent root = loader.load();
-            
-            // Cambiar la escena
-            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-            stage.setTitle("Acceso Clientes - Patagonia Wellboat");
-            stage.setScene(new Scene(root));
-            stage.show();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error al abrir Cliente: " + e.getMessage());
-        }
+
+    private static void cargarDatosIniciales(){
+        System.out.println("Generando datos iniciales...");
+        
+        listaUsuarios.add(new Gerente("admin", "admin123")); 
+        
+        // creamos un Asistente 
+        listaUsuarios.add(new Asistente("vendedor", "1234"));
+        
+        // creamos un cliente de prueba
+        listaUsuarios.add(new Usuario("cliente", "111", "Cliente"));
+
+        // Destinos
+        listaDestinos.add(new Destino("Calbuco", 15000, 4000));
+        listaDestinos.add(new Destino("Chonchi", 40000, 10000));
+        listaDestinos.add(new Destino("Maillen", 15000, 4000));
+        listaDestinos.add(new Destino("Isla Puluqui", 20000,5000));
+        listaDestinos.add(new Destino("Achao", 25000, 6000));
+        listaDestinos.add(new Destino("Quinchao", 25000, 6000));
+        listaDestinos.add(new Destino("Dalcahue", 30000, 8000));
+        listaDestinos.add(new Destino("Curaco de Vélez", 30000, 8000));
+        listaDestinos.add(new Destino("Queilen", 40000, 10000));
+        listaDestinos.add(new Destino("Chonchi", 40000, 10000));
+        listaDestinos.add(new Destino("Melinka", 50000, 12000));
+        listaDestinos.add(new Destino("Quellón", 50000, 12000));
+        // Flota inicial
+        listaEmbarcaciones.add(new CatamaranLiviano("CAT-001")); 
+        listaEmbarcaciones.add(new CatamaranLiviano("CAT-002"));
+        listaEmbarcaciones.add(new FerryMediano("FER-001"));
+
+        System.out.println("Datos iniciales listos.");
     }
-    
 }
